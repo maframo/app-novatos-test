f73d3830b25b675985dd91ba3eea474490792185
marc.franquesa <marc.franquesa@estudiantat.upc.edu>
2022-08-04 22:36:49 +0200

    Add more people, does not allow repeated names

--- source/home.py
+++ source/home.py
@@ -1,5 +1,6 @@
-from unicodedata import name
 import streamlit as st
+import gspread
+import pandas as pd
 
 def description():
     st.markdown(
@@ -35,14 +36,25 @@ def text_input():
 def finished():
     st.session_state.home_finished = True
 
+def exists(user):
+    gc = gspread.service_account_from_dict(st.secrets["gcp_service_account"])
+    sheet = gc.open_by_url(st.secrets["private_gsheets_url"]).worksheet("ranking")
+    df = pd.DataFrame(sheet.get_all_records())
+    dff = df[df["Nom"] == user]
+    return len(dff) > 0
+
 def main():
     description()
     user = text_input()
     if user != '':
         st.session_state.user = user
-    if "user" in st.session_state:
+
+    if "user" in st.session_state and exists(st.session_state.user):
+        st.error("Aquest nom ja s'està utilitzant!")
+
+    elif "user" in st.session_state:
         col1, col2 = st.columns(2)
-        col1.write(f"Ets el {st.session_state.user}?")
+        col1.write(f"Ets el/la {st.session_state.user}?")
         col2.button(
             "Si", 
             on_click=finished, 
